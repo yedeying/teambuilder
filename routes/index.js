6c113929f2818b256c518b940445e1652bcaa356
yedeying999 <kanwode918@qq.com>
2014-11-07 19:53:45 +0800

    2014/11/7 coding the email module

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,35 @@
 var express = require('express');
+var setting = require('../settings/global');
+var signup = require('../module/signup');
 var router = express.Router();
-
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  // TO DO
 });
+// get mothod
 router.get('/login', function(req, res) {
-  res.render('login', { title: 'Login' });
+  res.render('login', { title: '登录teambuilder' });
 });
+router.get('/signup', function(req, res) {
+  res.render('signup', { title: '注册teambuilder' });
+});
+router.get('/forget', function(req, res) {
+  res.render('forget', { title: '找回密码' });
+});
+// post mothod
+router.post('/signup', function(req, res) {
+  var email = req.body.email;
+  var name = req.body.username;
+  var password = req.body.password;
+  var time = new Date().getTime();
+  var password = signup.encodePassword(password, time);
+  if(signup.checkMail(email)) {
+    var tid = signup.saveTempInfo(email, name, time, password);
+    signup.sendMail(email, tid, res);
+  } else {
+    signup.postErr(res);
+  }
+});
+router.post('/checkemail', function(req, res) {
 
+})
 module.exports = router;
